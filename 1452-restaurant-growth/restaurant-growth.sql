fa8dea8ba3c228178a798343802efda47271c9cf
Elie TEILLET <54686553+eteillet@users.noreply.github.com>
2024-06-28 16:36:31 +0200

    Time: 348 ms (78.87%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1452-restaurant-growth/restaurant-growth.sql
@@ -0,0 +1,14 @@
+/* Write your T-SQL query statement below */
+WITH CTE AS (
+    SELECT  visited_on,
+            SUM(amount) AS amount
+    FROM Customer
+    GROUP BY visited_on
+)
+
+SELECT  visited_on,
+        SUM(amount) OVER (ORDER BY visited_on ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) AS amount,
+        ROUND(AVG(amount*1.00) OVER(ORDER BY visited_on ROWS BETWEEN 6 PRECEDING AND CURRENT ROW), 2) AS average_amount
+FROM CTE
+ORDER BY visited_on
+OFFSET 6 ROWS
